64b8791db589ad1e4c7631a969b647134a41f77f
Nicolas Cailotto <cailotto.nicoas@gmail.com>
2021-06-28 16:13:27 -0300

    Initial Commit

---
+++ src/main/java/com/mercadolibre/ipinfo/ipinfo/IpinfoApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.ipinfo.ipinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpinfoApplication.class, args);
+    }
+
+}
